b3e238e01b9de28e1f804fd9b0601a9f9293a506
Li Yanshun <liyanshun@megvii.com>
2017-07-26 13:17:24 +0800

    create project

---
+++ library/src/main/java/com/mushuichuan/library/NativeApi.java
@@ -0,0 +1,19 @@
+package com.mushuichuan.library;
+
+/**
+ * Created by liyanshun on 2017/7/26.
+ */
+
+public class NativeApi {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public static native String stringFromJNI();
+}
