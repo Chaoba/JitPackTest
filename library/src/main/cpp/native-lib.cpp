a72e87dfc3ae86c4136d1152bb4f374d718c5438
Li Yanshun <liyanshun@megvii.com>
2017-08-02 11:09:23 +0800

    add layer1 dependence

--- library/src/main/cpp/native-lib.cpp
+++ library/src/main/cpp/native-lib.cpp
@@ -1,11 +1,12 @@
 #include <jni.h>
 #include <string>
+#include "include/Layer1.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_mushuichuan_library_NativeApi_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = net_client_layer1.getResponse();
     return env->NewStringUTF(hello.c_str());
 }
